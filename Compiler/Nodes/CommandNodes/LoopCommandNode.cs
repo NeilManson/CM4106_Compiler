bacb101c5ca96502406b509a9417dc72a29043d3
Your name <1712991@rgu.ac.uk>
2022-11-23 17:26:06 +0000

    Add project files.

---
+++ Compiler/Nodes/CommandNodes/LoopCommandNode.cs
@@ -0,0 +1,42 @@
+﻿namespace Compiler.Nodes
+{
+    /// <summary>
+    /// A node corresponding to a while command
+    /// </summary>
+    public class LoopCommandNode : ICommandNode
+    {
+        /// <summary>
+        /// The condition associated with the loop
+        /// </summary>
+        public IExpressionNode Expression { get; }
+
+        /// <summary>
+        /// The command inside the loop
+        /// </summary>
+        public ICommandNode LoopCommand { get; }
+
+        /// <summary>
+        /// The command inside the loop
+        /// </summary>
+        public ICommandNode WhileCommand { get; }
+
+        /// <summary>
+        /// The position in the code where the content associated with the node begins
+        /// </summary>
+        public Position Position { get; }
+
+        /// <summary>
+        /// Creates a new while node
+        /// </summary>
+        /// <param name="expression">The condition associated with the loop</param>
+        /// <param name="command">The command inside the loop</param>
+        /// <param name="position">The position in the code where the content associated with the node begins</param>
+        public LoopCommandNode(ICommandNode command, IExpressionNode expression, ICommandNode command2,  Position position)
+        {
+            LoopCommand = command;
+            Expression = expression;
+            WhileCommand= command2;
+            Position = position;
+        }
+    }
+}
---
+++ Compiler/Nodes/CommandNodes/QuickIfCommandNode.cs
@@ -0,0 +1,37 @@
+﻿namespace Compiler.Nodes
+{
+    /// <summary>
+    /// A node corresponding to an if command
+    /// </summary>
+    public class QuickIfCommandNode : ICommandNode
+    {
+        /// <summary>
+        /// The condition expression
+        /// </summary>
+        public IExpressionNode Expression { get; }
+
+        /// <summary>
+        /// The do branch command
+        /// </summary>
+        public ICommandNode DoCommand { get; }
+
+        /// <summary>
+        /// The position in the code where the content associated with the node begins
+        /// </summary>
+        public Position Position { get; }
+
+        /// <summary>
+        /// Creates a new if command node
+        /// </summary>
+        /// <param name="expression">The condition expression</param>
+        /// <param name="thenCommand">The then branch command</param>
+        /// <param name="elseCommand">The else branch command</param>
+        /// <param name="position">The position in the code where the content associated with the node begins</param>
+        public QuickIfCommandNode(IExpressionNode expression, ICommandNode doCommand, Position position)
+        {
+            Expression = expression;
+            DoCommand = doCommand;
+            Position = position;
+        }
+    }
+}
